608752f23bb00cc6e8c0065b4c0b51040e23b27c
Josué Rivera <riguerjosh51@gmail.com>
2021-05-23 03:53:16 -0500

    login listo

--- inti-app/src/app/app.module.ts
+++ inti-app/src/app/app.module.ts
@@ -1,16 +1,23 @@
+import { LoginComponent } from './components/login/login.component';
+import { SharedModule } from './components/shared/shared.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//COMPONENTES
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
